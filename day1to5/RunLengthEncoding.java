40e6c92520932149d100af55d9699f46f59d1496
Deepak Sharma <dsharma04724@gmail.com>
2022-01-02 23:06:30 +0530

    Running Length Encoded

--- day1to5/RunLengthEncoding.java
+++ day1to5/RunLengthEncoding.java
@@ -22,7 +22,7 @@ public class RunLengthEncoding {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "wwwwaaadexxxxxxywww";
+		String str = "wwwwaaadexxxxxxxywww";
         System.out.println(printRLE(str));
 	}
 
